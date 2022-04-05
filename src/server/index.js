8d23cca956fee020aaf34ce1916db0e8aac295f0
Bruno Coutinho <xxbrunomassaxx@gmail.com>
2022-04-04 23:48:14 -0300

    Add cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,24 +1,13 @@
 const express = require("express")
 const teamsRoutes = require("./teams.routes")
+const cors = require("cors")
 const app = express()
 
 // Access Control
-app.use((req, res, next) => {
+app.use(cors())
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    
-    next();
-})
 app.use(express.json())
 app.use(teamsRoutes)
-
-
-app.get('/', (req, res) => {
-   return res.json("hi")
-})
 
 app.listen(3001, () => {
     console.log('Server is running')
